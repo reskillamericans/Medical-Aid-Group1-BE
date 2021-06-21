04bf10d789e800fd0817789b228fcc08b3c1b62b
David I <35611032+dikazoboh@users.noreply.github.com>
2021-06-21 11:43:05 -0400

    Added the FAQ management page

--- aidApp/models.py
+++ aidApp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.urls import reverse
+
 
 # Create your models here.
+
+class FAQ(models.Model):
+    question = models.CharField(max_length=400)
+    answer = models.CharField(max_length=400)
+    createdby = models.CharField(max_length=100)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.question
