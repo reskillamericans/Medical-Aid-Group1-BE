176ed0180701ab647316a9d06adacd3e3ed7faf5
chris franklin <cef1911@hotmail.com>
2021-06-25 16:25:22 -0400

    updated admin.py for models.py and database schema by chris f

--- aidApp/admin.py
+++ aidApp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Patient, Health_Practitioner, ManageProfile, ViewClinicsPharmInLocation, ViewHealthPractitioners, GiveFeedbackCompliants
+
 
 # Register your models here.
+admin.site.register(Patient)
+admin.site.register(Health_Practitioner)
+admin.site.register(ManageProfile)
+admin.site.register(ViewClinicsPharmInLocation)
+admin.site.register(ViewHealthPractitioners)
+admin.site.register(GiveFeedbackCompliants)
