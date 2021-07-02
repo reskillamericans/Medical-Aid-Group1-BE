6015736a09c78b851dbaaf7e1e50379bfbf58a20
michaleb <michale.bailey@gmail.com>
2021-07-02 13:51:36 -0400

    Added view for feedback

--- aidApp/views.py
+++ aidApp/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from .forms import FeedbackForm #,DocProfileForm, AppointmentForm
+from django.http import HttpResponse, HttpResponseRedirect
+#from . models import Health_Practitioner
+
+
 def index(request):
     return HttpResponse("<h1>Medical Aid app Homepage</h1>")
+
+
+def Feedback(request):
+    context = {}
+    submitted = False
+    
+    if request.method == 'POST':
+         form = FeedbackForm(request.POST)
+         if form.is_valid():
+            form.save()
+            
+            return HttpResponseRedirect('feedback?submitted=True')
+
+         else:
+            return render(request,'feedback.html',{'form': form})
+ 
+    context = {'form': FeedbackForm(),
+               'submitted': submitted
+              }
+    return render(request, 'feedback.html', context) 
+
