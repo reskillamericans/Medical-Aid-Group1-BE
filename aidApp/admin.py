fbc67e7b9a5fd16e0ef9b9a736a90ae31e7f9f25
David I <35611032+dikazoboh@users.noreply.github.com>
2021-07-10 15:03:07 -0400

    updates

--- aidApp/admin.py
+++ aidApp/admin.py
@@ -3,10 +3,8 @@ from django.contrib import admin
 from .models import FAQ
 
 # Register your models here.
-<<<<<<< HEAD
 
 admin.site.register(FAQ)
-=======
 admin.site.register(FAQ)
 admin.site.register(Feedback)
 admin.site.register(Patient)
@@ -14,4 +12,3 @@ admin.site.register(Health_Practitioner)
 admin.site.register(Clinic)
 admin.site.register(Pharmacy)
 admin.site.register(Appointment)
->>>>>>> 76cd49f86edb520f9b45e3e3de5ce1a76ab51f9f
