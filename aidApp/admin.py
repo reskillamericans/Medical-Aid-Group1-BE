8a05a5cc4386bcca822e8149b060ca4c2b37be50
David I <35611032+dikazoboh@users.noreply.github.com>
2021-07-10 21:44:25 -0400

    accepted changes to admin

--- aidApp/admin.py
+++ aidApp/admin.py
@@ -3,11 +3,6 @@ from django.contrib import admin
 from .models import FAQ
 
 # Register your models here.
-<<<<<<< HEAD
-
-admin.site.register(FAQ)
-=======
->>>>>>> 871c78bcc9e1369f8e334ea721a15063db51603b
 admin.site.register(FAQ)
 admin.site.register(Feedback)
 admin.site.register(Patient)
