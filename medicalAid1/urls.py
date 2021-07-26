357d3d69868e7a8716a0298be4e2120e1ea20203
michaleb <michale.bailey@gmail.com>
2021-07-26 18:30:33 -0400

    Adjusted aid/ path

--- medicalAid1/urls.py
+++ medicalAid1/urls.py
@@ -23,6 +23,6 @@ from aidApp import views as aid_app_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', aid_app_views.index, name = "homepage"),
-    path('aid/', include('aidApp.urls')),
+    path('aidApp/', include('aidApp.urls')),
     #path('faq/', include('faq.urls')),
 ]
