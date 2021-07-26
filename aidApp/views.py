38f0527a3f451bb2f372796ab4a6a1837591ef59
michaleb <michale.bailey@gmail.com>
2021-07-26 18:32:00 -0400

    Added contact view

--- aidApp/views.py
+++ aidApp/views.py
@@ -1,5 +1,48 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.mail import send_mail, get_connection
+from .forms import CreateContactForm #,DocProfileForm, AppCreateForm, AppUpdateForm, AppRetrieveForm 
+
+
 def index(request):
-    return HttpResponse("<h1>Medical Aid app Homepage</h1>")
+    return render(request, 'aidApp/contact/index.html') #, HttpResponse("<h1>Medical Aid app Homepage</h1>")
+
+
+def CreateContact(request):
+    
+    context = {}
+    submitted = False
+    
+    if request.method == 'POST':
+         form = CreateContactForm(request.POST)
+         if form.is_valid():
+             
+             subject = form.cleaned_data['subject']
+             message = form.cleaned_data['your_message']
+             sender = form.cleaned_data['your_email']
+             
+             administrators = []
+             for admin in User.objects.filter(is_superuser=True):
+                administrators.append(admin.email)
+            
+             con = get_connection('django.core.mail.backends.console.EmailBackend')
+             send_mail(subject,
+                      message,
+                      sender,
+                      administrators,
+                      connection=con            
+             )
+             form.save()
+             return HttpResponseRedirect('?submitted=True')
+         
+         else:
+             
+             return render(request,'aidApp/contact/contact.html',{'form': form, 'submitted': submitted})
+ 
+    context = {'form': CreateContactForm()}
+    return render(request, 'aidApp/contact/contact.html', context) 
+
+
+ 
