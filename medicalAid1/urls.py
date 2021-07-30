8ea849656a3e447c95668e5435a41c99f20af01c
Ennyola <medunoyeeni@gmail.com>
2021-07-30 16:07:36 +0100

    url

--- medicalAid1/urls.py
+++ medicalAid1/urls.py
@@ -21,5 +21,5 @@ from aidApp import views as aid_app_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', aid_app_views.index, name = "homepage"),
-    path('aid/', include('aidApp.urls'))
+    path('aid/', include('aidApp.urls')),
 ]
