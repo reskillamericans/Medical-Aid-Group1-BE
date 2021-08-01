7e5f7f53bcf3d766e8903c9be8dcf9a9781402e9
Promise Dada <gdadapromise@gmail.com>
2021-08-01 17:22:00 -0400

    Integrate Landing and About Us page

--- aidApp/views.py
+++ aidApp/views.py
@@ -9,7 +9,3 @@ def index(request):
 # About Us page 
 def about_us(request):
     return render(request, 'about-us.html')
-
-# ADMIN VIEWS
-def admin_views(request):
-    return render(request, 'Admin/admin-dash.html')
