374555434ece8b901f3bc0711fb0b25640196c0a
chris franklin <cef1911@hotmail.com>
2021-08-06 14:57:22 -0400

    updated FAQ page

--- aidApp/urls.py
+++ aidApp/urls.py
@@ -1,7 +1,10 @@
 from django import urls
 from django.urls import path
 from django.urls.conf import include
+from . import views
+
 
 urlpatterns = [
+    path('faq/', views.faq, name = 'faq')
     
 ]
--- aidApp/views.py
+++ aidApp/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import FAQ
 # Create your views here.
 def index(request):
     return HttpResponse("<h1>Medical Aid app Homepage</h1>")
+
+def faq(request):
+
+    faqs = FAQ.objects.all()
+    context = {
+        "Faqs":faqs
+
+    }
+    return render(request,'aidApp/faq.html',context)
