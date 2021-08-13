12f084ac37ec7f30dbd4d3ac24e346059666fabe
Ennyola <medunoyeeni@gmail.com>
2021-08-13 17:04:41 +0100

    updated urls

--- aidApp/urls.py
+++ aidApp/urls.py
@@ -5,9 +5,13 @@ from . import views
 from .views import (  
     support_view,
     support_success_view,
+    index,
+    about_us,
 )
 
 urlpatterns = [
+    path('', index, name="homepage"),
+    path('about-us/', about_us, name = "about-us"),
     path('support/', support_view, name = "support"),
     path('support-success/', support_success_view, name = "support-success"),
     path('faq/', views.faq, name = 'faq')
