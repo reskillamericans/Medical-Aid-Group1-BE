7a1e2c4fccda37a01925535526bc56039eddffeb
cef1911 <cef1911@hotmail.com>
2021-08-21 15:06:00 -0400

    error page updates

--- medicalAid1/settings.py
+++ medicalAid1/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+# SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'dtp9%-xx3=8v!(*le94$2g2#xu5^0q0$dvj6qe6t-gzozbcm4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
